1f0880273d7d88a65564543c669255cfd4bac014
Maksym Kulia <maksym.kulia@protonmail.com>
2021-02-27 00:58:17 +0200

    Initial

---
+++ tests/CoreTests.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MaksymKulia\WayForPayLaravel\Tests;
+
+/**
+ * Class CoreTests
+ * @package MaksymKulia\WayForPayLaravel\Tests
+ *
+ * @group core
+ */
+class CoreTests extends TestCase
+{
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     * @group core-simple
+     */
+    public function it_successfully_connects_to_live_api()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MaksymKulia\WayForPayLaravel\Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+/**
+ * Class TestCase
+ * @package MaksymKulia\WayForPayLaravel\Tests
+ */
+class TestCase extends OrchestraTestCase
+{
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getPackageProviders($app): void
+    {
+        // package providers
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        // perform environment setup
+    }
+}
