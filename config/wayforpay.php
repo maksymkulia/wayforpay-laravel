3470326443dfc9771f784bb0a6d6664f1fbc38e1
Maksym Kulia <maksym.kulia@protonmail.com>
2021-02-27 09:45:05 +0200

    add config & env

---
+++ config/wayforpay.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'testMode'        => env('WAYFORPAY_TESTMODE', true),
+    'merchantUri'     => env('WAYFORPAY_MERCHANT_URI', 'test.shop'),
+    'merchantAccount' => env('WAYFORPAY_MERCHANT_ACCOUNT', 'test_merch_n1'),
+    'merchantSecret'  => env('WAYFORPAY_MERCHANT_SECRET_KEY', 'flk3409refn54t54t*FNJRET'),
+];
--- src/WayForPayLaravelServiceProvider.php
+++ src/WayForPayLaravelServiceProvider.php
@@ -7,12 +7,56 @@ use Illuminate\Support\ServiceProvider;
 class WayForPayLaravelServiceProvider extends ServiceProvider
 {
     /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->configure();
+    }
+
+    /**
      * Get the services provided by the provider.
      *
      * @return array
      */
     public function provides(): array
     {
-        return ['wayforpaylaravel'];
+        return ['wayforpay'];
+    }
+
+    /**
+     * Setup the configuration.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/wayforpay.php', 'wayforpay'
+        );
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/wayforpay.php' => $this->app->configPath('wayforpay.php'),
+            ], 'wayforpay');
+        }
     }
 }
